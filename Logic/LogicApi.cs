744ddbb1cd15be36c8882dd09450e20bdc9eda1a
Oliwia Karbowa 242416 <242416@edu.p.lodz.pl>
2023-04-16 18:35:00 +0200

    Stage 1 beginning

---
+++ Logic/LogicApi.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public abstract class AbstractLogicApi
+    {
+        public static AbstractLogicApi CreateApi() 
+        {
+            return new LogicApi();
+        }
+    }
+
+    internal class LogicApi : AbstractLogicApi
+    {
+        public LogicApi()
+        {
+
+        }
+    }
+}
+
+
