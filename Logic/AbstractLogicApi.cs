fd518791112f79b58958d34260ab5c28ef10229c
Oliwia Karbowa 242416 <242416@edu.p.lodz.pl>
2023-04-16 20:23:29 +0200

    Creating MVVM

---
+++ Logic/AbstractLogicApi.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Logic
+{
+    public class AbstractLogicApi
+    {
+        public static AbstractLogicApi CreateApi()
+        {
+            return new LogicApi();
+        }
+
+        public abstract void CreateBall();
+
+    }
+
+    internal class LogicApi : AbstractLogicApi
+    {
+        public LogicApi() { }
+    }
+}
