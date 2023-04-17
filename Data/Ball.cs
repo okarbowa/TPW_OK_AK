e973c70994f57d6ee7fa7c711988500282daab89
Amelia Kwaśniewska <242446@edu.p.lodz.pl>
2023-04-17 23:32:24 +0200

    Added Ball

---
+++ Data/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class Ball : BallInterface
+    {
+        private double x { get; set; }
+        private double y { get; set; }
+        private double d { get; set; } = 10;
+        private double xVelocity { get; set; }
+        private double yVelocity { get; set; }
+
+
+        public Ball(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
