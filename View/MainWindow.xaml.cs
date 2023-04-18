4624f13055eee2776d15e14f921c179954a6ff1b
Oliwia Karbowa 242416 <242416@edu.p.lodz.pl>
2023-04-18 21:57:13 +0200

    ViewModel and GUI

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace View
         {
             InitializeComponent();
         }
+
+        private void numberOfBalls_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
--- ViewModel/WindowViewModel.cs
+++ ViewModel/WindowViewModel.cs
@@ -1,7 +1,57 @@
-﻿namespace ViewModel
-{
-    public class Class1
-    {
+﻿using Data;
+using Logic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
+namespace ViewModel
+{
+    public class WindowViewModel : INotifyPropertyChanged
+    {
+        private LogicAPI logicAPI;
+
+        public ICommand Apply { get; set; }
+        public ICommand Start { get; set; }
+        public ObservableCollection<Ball> ObservCollectionOfBall => logicAPI.getCollection();
+
+        public WindowViewModel()
+        {
+            logicAPI = LogicAPI.CreateAPI();
+            Apply = new Relay(() => logicAPI.CreateBall(numberOfBalls));
+            Start = new Relay(() => logicAPI.BallsMovement());
+        }
+
+        private int _numberOfBalls;
+        public int numberOfBalls
+        {
+            get { return _numberOfBalls; }
+            set
+            {
+                if (value != _numberOfBalls)
+                {
+                    _numberOfBalls = value;
+                    OnPropertyChanged("numberOfBalls");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property));
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
